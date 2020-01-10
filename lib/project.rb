c47799d68de3651de13981e8d80545ed8b384fb5
lioness3 <jcnailtech3@yahoo.com>
2020-01-10 13:05:46 -0800

    make instance of test fail

--- lib/project.rb
+++ lib/project.rb
@@ -6,5 +6,21 @@ def initialize(attributes)
   @id = attributes.fetch(:id)
 end
 
+def save
+  result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+  @id =result.first().fetch("id").to_i
+end
+
+def self.all
+  returned_projects = DB.exec("SELECT * FROM projects;")
+  projects = []
+  returned_projects.each() do |person|
+    title = person.fetch("title")
+    id = person.fetch("id").to_i
+    project_id = person.fetch("project_id").to_i
+    projects.push(Volunteer.new({:title => title, :id => id}))
+  end
+  projects
+end
 
 end
