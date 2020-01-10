dd6af2d0b605af5ea34f6e955674c2769efb12ab
lioness3 <jcnailtech3@yahoo.com>
2020-01-10 12:05:28 -0800

    make second test pass

--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -11,7 +11,7 @@ describe Volunteer do
   describe '#project_id' do
     it 'returns the project_id of the volunteer' do
       test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-      expect(test_volunteer.project_id).to eq 3
+      expect(test_volunteer.project_id).to eq 1
     end
   end
   #
